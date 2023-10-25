67b4a6c14b70bb6591474772f31346982204c5d0
MaybeRT-rt <maybe.rt@icloud.com>
2023-10-25 13:08:05 +0300

    Initial Commit

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by Liz-Mary on 25.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
